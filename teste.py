1122c8af93b2100f05c439967e4e6a1594247d3b
Mathieu <fabri.mathieu@free.fr>
2019-04-16 01:04:43 +0200

    teste définition lieu enregistrement

--- teste.py
+++ teste.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from __future__ import unicode_literals
 import youtube_dl
+import os
+import shutil
 
 class MyLogger(object):
     def debug(self, msg):
@@ -29,10 +31,13 @@ ydl_opts = {
 }
 
 while (1==1) :
-    url=input("Entrer l'url de la vidé don vous voulez récupérer le son: ")
+    url=input("Entrer l'url de la vidéo don vous voulez récupérer le son: ")
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+
+    # os.rename("*.mp3", "home/altins/Téléchargements/*.mp3")
+    
 
     cont=input("Voulez vous récupérer un autre son? O/n")
 
